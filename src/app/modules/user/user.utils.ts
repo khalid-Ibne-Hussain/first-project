45408ed0087dd920b152161e17590bb4d382d835
khalid-Ibne-Hussain <khalidtuhinbd@gmail.com>
2023-12-01 03:53:29 +0600

    id update

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -14,14 +14,30 @@ const findLastStudentId = async () => {
     .sort({ createdAt: -1 })
     .lean();
 
-  //   2023001  0001
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  //   2023  01  0001
+  // return lastStudent?.id ? lastStudent.id.subString(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 // year + semesterCode + 4digit number
 export const generateStudentId = async (payload: TAcademicSemester) => {
   // make first time 0000
-  const currentId = (await findLastStudentId()) || (0).toString();
+  let currentId = (0).toString();
+
+  const lastStudentId = await findLastStudentId(); //2023 01 0001
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6); // 01
+  const lastStudentYear = lastStudentId?.substring(0, 4); //2023
+  const currentSemesterCode = payload.code;
+  const currentYear = payload.year;
+
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentYear === currentYear
+  ) {
+    currentId = lastStudentId.substring(6); //0000
+  }
+
   let incrementId = (Number(currentId) + 1).toString().padStart(4, "0");
 
   incrementId = `${payload.year}${payload.code}${incrementId}`;
